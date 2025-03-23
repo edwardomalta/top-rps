327220a1db044cf75ed1d6268fe9afac6afca1aa
Edwardo Malta <edwardoalta@gmail.com>
2025-03-22 20:02:55 -0600

    Solving the most elemental problem

---
+++ rps.js
@@ -0,0 +1,11 @@
+/* 
+    Paso uno: Describir el problema.
+    Crear una funcion llamada getComputerChoice, 
+    retornando aleatoriamente "Rcok", "Paper", "Scissors"
+ */
+
+function getComputerChoice() {
+    return "scissors";
+}
+
+getComputerChoice();
